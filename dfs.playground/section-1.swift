7cb72826e581fe81ab3a2701acbae3f89abfb9e2
Tianyu Liu <poohlty@sina.com>
2014-06-21 21:23:46 +0900

    Simple DFS now working

--- dfs.playground/section-1.swift
+++ dfs.playground/section-1.swift
@@ -2,7 +2,34 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+var str = "Hello, Swift!"
 
-println(str)
+// Global array to note if we have visited a node or not
+var visited = Array(count: 6, repeatedValue: false)
 
+// A very crude representation of a graph, to be improved later
+var graph = [[1,3],[2,3],[3],[1],[],[2,4]]
+
+func search(v:Int) {
+    if visited[v] == false{
+        println("\(v) is reached")
+        visited[v] = true
+        
+        for d in graph[v]{
+            if visited[d] == false {
+                search(d)
+            }
+        }
+    } else {
+        println("All connected vertices reached")
+    }
+}
+
+func DFSearch(g:NSArray) {
+    for v in 0..g.count {
+        println("Starting from \(v)")
+        search(v)
+    }
+}
+
+DFSearch(graph)
